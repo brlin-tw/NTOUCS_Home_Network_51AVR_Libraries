9964a7a43f36a7f3b2c035895e7d1d38fcd2d54b
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2012-12-09 17:32:36 +0800

    [ADC]更新系統初始化函式，關掉更多不必要的控制訊號

--- ADC/Source_code/main.c
+++ ADC/Source_code/main.c
@@ -23,8 +23,8 @@ main.c
 /*||||| Definition of data type, enumeration, data structure and class |||||*/
 
 /*||||| 函式雛型 | Function Prototypes |||||*/
-	/* 初始化硬體（關掉所有設備信號）的函式 */
-	void initialize();
+	/* 初始化硬體（關掉所有元件）的函式 */
+		void initializeSystem();
 
 /*||||| 全域變數 | Global Variables |||||*/
 
@@ -32,7 +32,7 @@ main.c
 /* 程式進入點 | Program entry point
    　因為嵌入式系統開機之後就會執行到電源關閉，故不需要回傳值*/
 void main(void){
-	initialize();
+	initializeSystem();
 	
 	/* main loop */
 	while(TRUE){
@@ -43,8 +43,20 @@ void main(void){
 	return;
 }
 
-void initialize(){
+void initializeSystem(){
+	/* 清空 LED 輸出 */
+		LED = 0xFF;
 	/* 停用 ADC 的輸出 */
-		adc_read_bar_cs_bar = LOGIC_HIGH;
+		adc_chip_select_bar_read_bar = LOGIC_HIGH;
+	/* 停用 DIP 的輸出 */
+		dip_sw_chip_enable_bar = LOGIC_HIGH;
+	/* 停用 LCD */
+		lcd_enable = LOGIC_LOW;
+	/* 停用 LCD 的暫存器輸入 */
+		lcd_read_write_bar = LOGIC_HIGH;
+	/* 停用 7 段顯示器的輸入 */
+		SEVEN_SEG = 0x00;
+		seven_seg_latch_position_enable = LOGIC_HIGH;
+		seven_seg_latch_position_enable = LOGIC_LOW;
 	return;
 }
